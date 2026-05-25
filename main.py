5eb28cf71b35d1700498758d8203f61281c1368e
martinshi <martinshi1206@hotmail.com>
2026-05-25 15:46:57 +0800

    update main.py

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ from crawl import BiliCrawler
 FILE_PATH = 'rat/public/data/BinData/'
 
 e = ETLer(root=FILE_PATH)
-e.export_color_html()
+#e.export_color_html()
 e.load_sqlite()
 
-c = BiliCrawler('data/a.pkl')
-c.test()
+#c = BiliCrawler('data/tmp.pkl')
+#c.test()
